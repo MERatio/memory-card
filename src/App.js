9adbb8ce1af00ce4b6d5aa9864d09801c6f9feab
Mark Edward Ratio <markedwardratio@gmail.com>
2021-02-20 13:18:33 +0800

    Fetch all pokemons and pick 20 random pokemons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { fetchPokemons, getRandomElements } from './lib/helpers';
 import './App.css';
 
-function App() {
-  return (
-    <main>
-      <h1>Memory Card</h1>
-    </main>
-  );
+function App(props) {
+	const [pickedPokemons, setPickedPokemons] = useState([]);
+
+	async function newGame() {
+		const pokemons = await fetchPokemons();
+		const randomPokemons = getRandomElements(pokemons, 20);
+		setPickedPokemons(randomPokemons);
+	}
+
+	useEffect(() => {
+		newGame();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<main>
+			<div>{JSON.stringify(pickedPokemons)}</div>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/lib/helpers.js
@@ -0,0 +1,40 @@
+async function fetchPokemons() {
+	try {
+		const url = 'https://pokeapi.co/api/v2/pokemon?limit=1118';
+		const response = await fetch(url, { mode: 'cors' });
+		const data = await response.json();
+		return data.results;
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+// https://stackoverflow.com/a/2450976
+// WARNING! this mutates the original array.
+function shuffle(array) {
+	let currentIndex = array.length;
+	let temporaryValue;
+	let randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+function getRandomElements(array, num) {
+	const arrayCopy = [...array];
+	shuffle(arrayCopy);
+	return arrayCopy.slice(0, num);
+}
+
+export { fetchPokemons, getRandomElements };
